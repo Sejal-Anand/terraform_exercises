ed7628dd77677887e6f3d8723ca4888ed48b16d2
Sejal-Anand <sejalanand2.10@gmail.com>
2024-01-22 12:43:57 +0530

    Security groups linked

--- exercise_0100/ec2.tf
+++ exercise_0100/ec2.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "web" {
   ami           = "ami-0d3f444bc76de0a79"
   instance_type = "t2.micro"
+  security_groups = [aws_security_group.sec_ssh.name, aws_security_group.sec_http.name]
 
   tags = {
     Name = "exercise_0100"
